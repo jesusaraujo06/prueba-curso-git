bc0b1dd6c94ee89682dad90a8bf60a2666b9781d
Jesus Araujo <jesusaraujo.0699@gmail.com>
2021-12-28 15:45:01 -0500

    Insercion de estructura html y formulario en el archivo index.php

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
-<?php
+<?php 
+
+require 'menu.php'; 
 
 $arrayExample = [
     [
         'nombre' => 'Jesus', 
-        'Apellido' => 'Araujo'
+        'apellido' => 'Araujo'
     ],
     [
         'nombre' => 'Steven', 
-        'Apellido' => 'Carrascal'
+        'apellido' => 'Carrascal'
     ],
 ];
 
 for ($i=0; $i < count($arrayExample); $i++) { 
-    echo "Hola mi nombre es " . $arrayExample[$i]['nombre'];
+    echo "Hola mi nombre es " . $arrayExample[$i]['nombre'] . "<br>";
+    echo "Hola mi apellido es " . $arrayExample[$i]['apellido'] . "<br>";
 }
 
+function getArray($array){
+    echo $array;
+}
 
-// echo $arrayExample[0]['nombre'];
+?>
 
+<form action="">
+    <p>Username <input type="text"></p>
+    <p>Password <input type="password"></p>
+    <p><input type="submit" value="Enviar"></p>
+</form>
